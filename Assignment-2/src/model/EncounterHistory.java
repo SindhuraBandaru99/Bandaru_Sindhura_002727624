e1006ff1d537b5843186a97e6eb5f71ecf4d330c
Sindhura Bandaru <bandaru.si@northeastern.edu>
2022-10-26 11:46:36 -0400

    Role Functionalities Added.

---
+++ Assignment-2/src/model/EncounterHistory.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.List;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sindhura Bandaru
+ */
+public class EncounterHistory {
+    private List<Encounter> listOfEncounters;
+}
---
+++ Assignment-2/src/model/HospitalDirectory.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sindhura Bandaru
+ */
+public class HospitalDirectory {
+    private ArrayList<Hospital> listOfHospitals;  
+
+    public HospitalDirectory() {
+        listOfHospitals = new ArrayList<Hospital>();
+    }
+
+    public HospitalDirectory(ArrayList<Hospital> listOfHospitals) {
+        this.listOfHospitals = listOfHospitals;
+    }
+
+    public List<Hospital> getListOfHospitals() {
+        return listOfHospitals;
+    }
+
+    public void setListOfHospitals(ArrayList<Hospital> listOfHospitals) {
+        this.listOfHospitals = listOfHospitals;
+    }
+}
